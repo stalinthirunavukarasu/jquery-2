ccb243a6d1d2efa660eb277ee09a051d6c1dcfc1
stalin <vickypro565@gmail.com>
2025-11-11 16:27:14 +0530

    Add files via upload

---
+++ ulib/ujquery.js
@@ -0,0 +1,4 @@
+document.write("<script src='/zlib/jquery.ui.themes.1.14.1/jquery-ui.min.css'></script>");
+document.write("<script src='/zlib/jquery.color.3.0.0/jquery.color-3.0.0.min.js'></script>");
+document.write("<script src='/zlib/jquery.core.3.7.1/jquery-3.7.1.min.js'></script>");
+document.write("<script src='/zlib/jquery.ui.1.14.1/jquery-ui.min.js'></script>");
