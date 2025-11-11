89e7691330759bfc47c62f5f0d98187d21e41986
stalin <vickypro565@gmail.com>
2025-11-11 16:29:31 +0530

    Replace local jQuery scripts with CDN link

--- ulib/ujquery.js
+++ ulib/ujquery.js
@@ -1,4 +1,1 @@
-document.write("<script src='/zlib/jquery.ui.themes.1.14.1/jquery-ui.min.css'></script>");
-document.write("<script src='/zlib/jquery.color.3.0.0/jquery.color-3.0.0.min.js'></script>");
-document.write("<script src='/zlib/jquery.core.3.7.1/jquery-3.7.1.min.js'></script>");
-document.write("<script src='/zlib/jquery.ui.1.14.1/jquery-ui.min.js'></script>");
+document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'></script>");
